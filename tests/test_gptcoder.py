5211181568927e63ae086125d338c224d3988ec2
joshforlenza <joshforlenza@gmail.com>
2024-03-26 13:56:45 -0400

    Added readability tests

--- tests/test_gptcoder.py
+++ tests/test_gptcoder.py
@@ -81,6 +81,28 @@ class Tests:
                 output = gptcoder.get_commenting()
                 assert(1 <= output <= 10), f"Expected get_commenting() to return an int between 10 and 100. Instead, it returned {output}"
 
+    class Test_get_readability:
+
+        @pytest.mark.parametrize("input_value", [(2)])
+        def test_output(self, input_value):
+            with patch('builtins.input', return_value=input_value):
+                output = gptcoder.get_readability()
+                assert output is not None, "Expected get_readability() to return output. Instead, it returned None"
+        
+        @pytest.mark.parametrize("input_value", [(2)])
+        def test_is_int(self, input_value):
+            with patch('builtins.input', return_value=input_value):
+                output = gptcoder.get_readability()
+                assert isinstance(
+                output, int
+            ), f"Expected get_readability() to return an int. Instead, it returned {output}"
+
+        @pytest.mark.parametrize("input_value", [(2)])
+        def test_is_in_range(self, input_value):
+            with patch('builtins.input', return_value=input_value):
+                output = gptcoder.get_readability()
+                assert(1 <= output <= 10), f"Expected get_readability() to return an int between 10 and 100. Instead, it returned {output}"
+
         
         
         
